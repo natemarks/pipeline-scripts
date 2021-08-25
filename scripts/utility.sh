2470ea90df6c26255dc261feb8fbfccab0bf71d9
Nate Marks <npmarks@gmail.com>
2021-08-25 04:56:48 -0400

    fix: Troubleshooting sourcing utility

--- scripts/utility.sh
+++ scripts/utility.sh
@@ -1,27 +1,5 @@
 #!/usr/bin/env bash
 
-#######################################
-# use assume role and set the temporary credentials
-#######################################
-function awsCreds() {
-  local ACCOUNT="${1}"
-  local ROLE_NAME="${2}"
-  local SESSION_NAME="${3}"
-  echo "## Assuming a role"
-  creds=$(aws sts assume-role --role-arn "arn:aws:iam::${ACCOUNT}:role/${ROLE_NAME}" --role-session-name "${SESSION_NAME}")
-  AWS_ACCESS_KEY_ID=$(echo "$creds" | jq -r .Credentials.AccessKeyId)
-  export AWS_ACCESS_KEY_ID
-  AWS_SECRET_ACCESS_KEY=$(echo "$creds "| jq -r .Credentials.SecretAccessKey)
-  export AWS_SECRET_ACCESS_KEY
-  AWS_SESSION_TOKEN=$(echo "$creds "| jq -r .Credentials.SessionToken)
-  export AWS_SESSION_TOKEN
-  # echo "fetching credentials"
-  # creds=$(aws-vault exec -j $VAULT_PROFILE)
-  # export AWS_ACCESS_KEY_ID=$(echo "$creds" | jq -r .AccessKeyId)
-  # export AWS_SECRET_ACCESS_KEY=$(echo "$creds" | jq -r .SecretAccessKey)
-  # export AWS_SESSION_TOKEN=$(echo "$creds "| jq -r .SessionToken)
-}
-
 
 #######################################
 # test Function export
