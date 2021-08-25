2653d9af0dc34e6c2f0aed9eb3b5f54c05629f7b
Nate Marks <npmarks@gmail.com>
2021-08-25 04:43:04 -0400

    fix: Resolve shellcheck errors in scripts/utility.sh

--- scripts/utility.sh
+++ scripts/utility.sh
@@ -9,10 +9,13 @@ function awsCreds() {
   local ROLE_NAME="${2}"
   local SESSION_NAME="${3}"
   echo "## Assuming a role"
-  creds=$(aws sts assume-role --role-arn "arn:aws:iam::${ACCOUNT}:role/${ROLE_NAME}" --role-session-name ${SESSION_NAME})
-  export AWS_ACCESS_KEY_ID=$(echo "$creds" | jq -r .Credentials.AccessKeyId)
-  export AWS_SECRET_ACCESS_KEY=$(echo "$creds "| jq -r .Credentials.SecretAccessKey)
-  export AWS_SESSION_TOKEN=$(echo "$creds "| jq -r .Credentials.SessionToken)
+  creds=$(aws sts assume-role --role-arn "arn:aws:iam::${ACCOUNT}:role/${ROLE_NAME}" --role-session-name "${SESSION_NAME}")
+  AWS_ACCESS_KEY_ID=$(echo "$creds" | jq -r .Credentials.AccessKeyId)
+  export AWS_ACCESS_KEY_ID
+  AWS_SECRET_ACCESS_KEY=$(echo "$creds "| jq -r .Credentials.SecretAccessKey)
+  export AWS_SECRET_ACCESS_KEY
+  AWS_SESSION_TOKEN=$(echo "$creds "| jq -r .Credentials.SessionToken)
+  export AWS_SESSION_TOKEN
   # echo "fetching credentials"
   # creds=$(aws-vault exec -j $VAULT_PROFILE)
   # export AWS_ACCESS_KEY_ID=$(echo "$creds" | jq -r .AccessKeyId)
