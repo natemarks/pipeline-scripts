0ec9f9405d21bf876f20a6ea00e7b4029d785583
Nate Marks <npmarks@gmail.com>
2021-08-28 06:15:31 -0400

    feat: Add scripts/destroy_terraform.sh

---
+++ scripts/destroy_terraform.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+# https://gist.github.com/natemarks/aebb7e84010d4bc37270d554106cb38b
+set -Eeuo pipefail
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+INITIAL_WD="$(pwd)"
+
+usage() {
+  cat <<EOF
+Usage: destroy_terraform.sh [-h] [-v] -t tf_module
+
+Change directory to the terraform module directory, run terraform destroy. Finally change back to the original directory
+
+Available options:
+
+-h, --help      Print this help and exit
+-v, --verbose   Print script debug info
+-t, --tf_module terraform module directory
+EOF
+  exit
+}
+
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+  cd "${INITIAL_WD}"
+}
+
+setup_colors() {
+  if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
+    NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
+  else
+    # shellcheck disable=SC2034
+    NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
+  fi
+}
+
+msg() {
+  echo >&2 -e "${1-}"
+}
+
+die() {
+  local msg=$1
+  local code=${2-1} # default exit status 1
+  msg "$msg"
+  exit "$code"
+}
+
+parse_params() {
+  # default values of variables set from params
+  tf_module=''
+
+  while :; do
+    case "${1-}" in
+    -h | --help) usage ;;
+    -v | --verbose) set -xv ;;
+    --no-color) NO_COLOR=1 ;;
+    -t | --tf_module)
+      tf_module="${2-}"
+      shift
+      ;;
+    -?*) die "Unknown option: $1" ;;
+    *) break ;;
+    esac
+    shift
+  done
+
+  # check required params and arguments
+  [[ -z "${tf_module-}" ]] && die "Missing required parameter: tf_module"
+
+  return 0
+}
+
+parse_params "$@"
+setup_colors
+
+msg "initial working directory: ${INITIAL_WD}"
+msg "terraform module directory: ${tf_module}"
+msg "terraform executable: $(which terraform)"
+cd "${tf_module}"
+terraform init && terraform plan && terraform destroy -auto-approve
